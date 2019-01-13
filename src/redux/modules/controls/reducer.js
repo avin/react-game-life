81fad6dc93a8f49ee97c7c937377c1cc3aa6625b
avin <carcinogen75@gmail.com>
2019-01-13 20:14:44 +0300

    fix restore params

--- src/redux/modules/controls/reducer.js
+++ src/redux/modules/controls/reducer.js
@@ -2,21 +2,12 @@ import { UPDATE_CONTROLS } from './actionTypes';
 
 const initialState = {
     started: false,
-    cellSize: 10,
-    speed: 50,
-    randomAreaSize: 20,
-    randomAreaPopulation: 5,
-    renderMode: 'html',
+    cellSize: +(localStorage.getItem(`life:cellSize`) || 10),
+    speed: +(localStorage.getItem(`life:speed`) || 50),
+    randomAreaSize: +(localStorage.getItem(`life:randomAreaSize`) || 20),
+    randomAreaPopulation: +(localStorage.getItem(`life:randomAreaPopulation`) || 5),
+    renderMode: localStorage.getItem(`life:randomAreaPopulation`) || 'html',
 };
-
-Object.keys(initialState).forEach(key => {
-    if (!['started'].includes(key)) {
-        const savedValue = localStorage.getItem(`life:${key}`);
-        if (savedValue !== null) {
-            initialState[key] = savedValue;
-        }
-    }
-});
 
 export default function reducer(state = initialState, action = {}) {
     switch (action.type) {
