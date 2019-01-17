8245617a09b8a3adbd79fd89ebf72e32ec292450
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-17 09:48:11 +0000

    Deploy avin/react-game-life to github.com/avin/react-game-life.git:gh-pages

---
+++ precache-manifest.903adae52fd8e85fb5ad4196994ccb2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ed402fc1c1f5e1b9857",
+    "url": "./static/css/main.925db20c.chunk.css"
+  },
+  {
+    "revision": "5ed402fc1c1f5e1b9857",
+    "url": "./static/js/main.5ed402fc.chunk.js"
+  },
+  {
+    "revision": "8577240280997ee8f297",
+    "url": "./static/css/1.87253ada.chunk.css"
+  },
+  {
+    "revision": "8577240280997ee8f297",
+    "url": "./static/js/1.85772402.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "00cbfaf71881ff52965c7090f6cc367e",
+    "url": "./index.html"
+  }
+];
